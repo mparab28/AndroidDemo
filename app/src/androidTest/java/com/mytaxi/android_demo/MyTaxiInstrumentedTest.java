9b872586c2ff1607181d292a04a05aced81dc3af
Mrunali Parab <kartikeya.verma@cimpress.com>
2018-09-20 00:13:26 +0530

    Changes to get Driver from autocomplete list using name

--- app/src/androidTest/java/com/mytaxi/android_demo/MyTaxiInstrumentedTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/MyTaxiInstrumentedTest.java
@@ -16,7 +16,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.security.acl.Permission;
 import java.util.concurrent.TimeUnit;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
@@ -27,10 +26,12 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.mytaxi.android_demo.util.AndroidHasProperty.hasPropertyAndroid;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
@@ -40,6 +41,7 @@ public class MyTaxiInstrumentedTest {
     private static final String USER_NAME = "crazydog335";
     private static final String PASSWORD = "venture";
     private static final String SEARCH_TEXT = "sa";
+    private static final String DRIVER_NAME = "Sarah Scott";
 
     @Rule // create the main activity rule to initialize the application
     public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -55,6 +57,7 @@ public class MyTaxiInstrumentedTest {
 
     @Test
     public void shouldLoginToAppAndSearchForDriver() throws InterruptedException {
+
         // find login page fields, validate and perform actions (enter text and click on button)
         onView(withId(R.id.edt_username)).check(matches(isEnabled())).perform(typeText(USER_NAME));
         onView(withId(R.id.edt_password)).check(matches(isEnabled())).perform(typeText(PASSWORD), closeSoftKeyboard());
@@ -62,15 +65,19 @@ public class MyTaxiInstrumentedTest {
 
         TimeUnit.SECONDS.sleep(3); // wait for application to navigate to next page
 
+        // enter text('sa') in search text field and then click on driver searching it through the name ('Sarah Scott')
         onView(withId(R.id.textSearch)).check(matches(isEnabled())).perform(typeText(SEARCH_TEXT));
-        onDriverSearch().check(matches(withText("Sarah Scott"))).perform(click());
+        onDriverSearch(DRIVER_NAME).check(matches(isDisplayed())).perform(click());
 
-        TimeUnit.SECONDS.sleep(2); // wait to manually validate the successful login
+
+        TimeUnit.SECONDS.sleep(2); // wait to manually validate the final step on emulator
     }
 
-    // Returns the 2nd driver from DriverAdapter List
-    private static DataInteraction onDriverSearch() {
-        return onData(allOf(is(instanceOf(Driver.class)))).inRoot(RootMatchers.isPlatformPopup()).atPosition(1);
+    // Returns the driver from DriverAdapter List based on the name that is passed in method parameters
+    private static DataInteraction onDriverSearch(final String driverName) {
+        //return onData(allOf(is(instanceOf(Driver.class)))).inRoot(RootMatchers.isPlatformPopup()).atPosition(1);
+        return onData(allOf(is(instanceOf(Driver.class)), hasPropertyAndroid("mName", equalTo(driverName))))
+                .inRoot(RootMatchers.isPlatformPopup());
     }
 
 }
---
+++ app/src/androidTest/java/com/mytaxi/android_demo/util/AndroidHasProperty.java
@@ -0,0 +1,60 @@
+package com.mytaxi.android_demo.util;
+
+import android.util.Log;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.lang.reflect.Field;
+
+
+/**
+ * Custom Matcher Implementation; Code copied from HasPropertyWithValue matcher;
+ *
+ * Reason to create this custom matcher implementation: Was getting an error
+ * 'java.lang.NoClassDefFoundError: Failed resolution of: Ljava/beans/Introspector;'
+ *
+ * The problem is not technically a hamcrest issue,
+ * but an issue with android that it does not implement all of the java.beans classes,
+ * such as PropertyUtils which Hamcrest makes use of. Worked around it by creating an android-specific
+ * hasProperty matcher that works based on pure reflection instead of using PropertyUtils
+ *
+ * Please refer to: https://groups.google.com/forum/#!msg/android-test-kit-discuss/5w1bhljm3ag/ht-txuQ3O8YJ
+ *
+ * @param <T>
+ */
+public class AndroidHasProperty<T> extends TypeSafeMatcher<T> {
+    private String propertyName;
+    private Matcher<Object> valueMatcher;
+
+    @SuppressWarnings("unchecked")
+    private AndroidHasProperty(String propertyName, Matcher<?> valueMatcher) {
+        this.propertyName = propertyName;
+        this.valueMatcher = (Matcher<Object>) valueMatcher;
+    }
+
+    @Override
+    protected boolean matchesSafely(T item) {
+        try {
+            Field propertyField = item.getClass().getDeclaredField(propertyName);
+            propertyField.setAccessible(true);
+            Object value = propertyField.get(item);
+            return valueMatcher.matches(value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.w("Custom matcher error", "No matching field found with property name: " + propertyName);
+        }
+        return false;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("hasPropertyAndroid(")
+                .appendValue(propertyName).appendText(", ")
+                .appendDescriptionOf(valueMatcher).appendText(")");
+    }
+
+    public static Matcher<Object> hasPropertyAndroid(String propertyName, Matcher<?> valueMatcher) {
+        return new AndroidHasProperty<>(propertyName, valueMatcher);
+    }
+}
