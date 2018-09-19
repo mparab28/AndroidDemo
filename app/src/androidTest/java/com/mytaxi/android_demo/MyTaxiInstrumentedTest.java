943d50217e8a40761bf00995c3c6fd0ee1514fbe
Mrunali Parab <kartikeya.verma@cimpress.com>
2018-09-19 12:27:00 +0530

    added test rule dependency; create new test class for espresso test

---
+++ app/src/androidTest/java/com/mytaxi/android_demo/MyTaxiInstrumentedTest.java
@@ -0,0 +1,10 @@
+package com.mytaxi.android_demo;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MyTaxiInstrumentedTest {
+
+}
